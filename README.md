5ae92cc5cb1b967e94f4bcdb86c28f8ad31967a9
deepGio <giovanni.nardini1990@gmail.com>
2020-03-07 18:55:33 +0100

    version1.0

--- README.md
+++ README.md
@@ -12,10 +12,25 @@ There are two main parts within the project:
     4. Keras model building and training
     5. Result Visualization
     6. Model saving
-* A python script `draw_your_own.py` which enables a canvas interface with tkinter in order to test the model with live drawing.
+* A python script `draw_your_own.py` which enables a canvas interface with tkinter in order to test the model with live drawing.  
+    Command: `python draw_your_own.py --model_name sketch2fruits` (the argument model_name should contain the name of the saved model without any extensions.
 
 The network architecture that best fits the data is the following:
 
 ![architecture](images/network_architecture.png)
 
+<br>
 
+##### Requirements
+
+All the modules have been tested with 
+`Python 3.6`
+and the following packages versions:
+```
+numpy==1.14.5  
+opencv-python==4.1.0.25  
+matplotlib==3.0.3  
+tensorflow-gpu==1.10.0  
+jupyter==1.0.0  
+Pillow==5.3.0  
+```
--- draw_your_own.py
+++ draw_your_own.py
@@ -3,16 +3,11 @@ from tkinter.colorchooser import askcolor
 import PIL
 import numpy as np
 import cv2
-from PIL import Image, ImageDraw
-import matplotlib.pyplot as plt
+from PIL import Image, ImageDraw, ImageTk
 from tensorflow.keras.models import model_from_json
 from tensorflow.keras.backend import set_session
 import tensorflow as tf
-config = tf.ConfigProto()
-config.gpu_options.allow_growth = True  # dynamically grow the memory used on the GPU
-config.log_device_placement = True  # to log device placement (on which device the operation ran)
-sess = tf.Session(config=config)
-set_session(sess) 
+import argparse
 
 
 class Paint(object):
@@ -20,35 +15,32 @@ class Paint(object):
     DEFAULT_PEN_SIZE = 5.0
     DEFAULT_COLOR = 'black'
 
-    def __init__(self):
-        self.root = Tk()
+    def __init__(self, loaded_model):
+        self.root = Tk(className=' SKETCH2FRUITS: turn your sketches into fruits...')
 
         self.pen_button = Button(self.root, text='pen', command=self.use_pen)
         self.pen_button.grid(row=0, column=0)
 
-        self.brush_button = Button(self.root, text='brush', command=self.use_brush)
-        self.brush_button.grid(row=0, column=1)
-
-        self.color_button = Button(self.root, text='color', command=self.choose_color)
-        self.color_button.grid(row=0, column=2)
-
         self.eraser_button = Button(self.root, text='eraser', command=self.use_eraser)
-        self.eraser_button.grid(row=0, column=3)
+        self.eraser_button.grid(row=0, column=1)
 
-        self.choose_size_button = Scale(self.root, from_=1, to=10, orient=HORIZONTAL)
-        self.choose_size_button.grid(row=0, column=4)
+        self.choose_size_button = Scale(self.root, from_=5, to=7, orient=HORIZONTAL)
+        self.choose_size_button.grid(row=0, column=2)
 
         self.c = Canvas(self.root, bg='white', width=600, height=600)
         self.c.grid(row=1, columnspan=5)
+
+        self.d = Canvas(self.root, bg='white', width=600, height=600)
+        self.d.grid(row=1, column=5, columnspan=5)
 
         self.image1 = PIL.Image.new('RGB', (600, 600), 'white')
         self.draw = ImageDraw.Draw(self.image1)
 
         self.clear_canvas = Button(self.root, text='CLEAR', command=self.clear_canvas)
-        self.clear_canvas.grid(row=0, column=5)
+        self.clear_canvas.grid(row=0, column=3)
 
         self.setup()
-        self.load_model()
+        self.loaded_model = loaded_model
         self.first_prediction = True
         self.root.mainloop()
 
@@ -57,19 +49,11 @@ class Paint(object):
         self.c.grid(row=1, columnspan=5)
         self.image1 = PIL.Image.new('RGB', (600, 600), 'white')
         self.draw = ImageDraw.Draw(self.image1)
+        if not self.first_prediction:
+            white_img = np.zeros((200,200,3), dtype='uint8')
+            img = ImageTk.PhotoImage(image=Image.fromarray(white_img))
+            self.d.itemconfig(self.image_on_d, image=img)
         self.setup()
-
-    def load_model(self):
-        with open('models/fruits7.json', 'r') as file:
-            loaded_json = file.read()
-        # clean json:
-        loaded_json = loaded_json.replace(", \"interpolation\": \"nearest\"", '')
-        loaded_json = loaded_json.replace(", \"output_padding\": null", '')
-        loaded_json = loaded_json.replace(", \"ragged\": false", '')
-        loaded_json = loaded_json.replace(", \"kernel_initializer\": {\"class_name\": \"GlorotUniform\", \"config\": {\"seed\": null, \"dtype\": \"float32\"}}", '')
-        self.loaded_model = model_from_json(loaded_json)
-        self.loaded_model.load_weights('models/fruits7.h5')
-        self.loaded_model.compile(loss='mse', optimizer='adam')
 
     def setup(self):
         self.old_x = None
@@ -83,9 +67,6 @@ class Paint(object):
 
     def use_pen(self):
         self.activate_button(self.pen_button)
-
-    def use_brush(self):
-        self.activate_button(self.brush_button)
 
     def choose_color(self):
         self.eraser_on = False
@@ -108,7 +89,6 @@ class Paint(object):
                                width=self.line_width, fill=paint_color,
                                capstyle=ROUND, smooth=TRUE, splinesteps=36)
             self.draw.line((self.old_x, self.old_y, event.x, event.y), fill=paint_color, width=self.line_width)
-            #self.predict()
         self.old_x = event.x
         self.old_y = event.y
 	
@@ -118,20 +98,16 @@ class Paint(object):
         resized[resized <= 0.5] = 0.0
         preprocessed = 1 - resized
         prediction = self.loaded_model.predict(preprocessed.reshape(1,200,200,1)).reshape(200,200,3)
-        prediction = prediction/np.max(prediction)
-        sketch = cv2.resize(np.array(self.image1, dtype='float'), (200,200))
-        sketch = sketch/np.max(sketch)
-        merge = prediction + (sketch*0.2)
-        print(sketch)
-        print(prediction)
+        prediction = np.array((prediction/np.max(prediction))*255, dtype='uint8')
+        prediction = cv2.resize(prediction, (400,400))
+        img = ImageTk.PhotoImage(image=Image.fromarray(prediction))
         if self.first_prediction:
-            plotting = plt.imshow(prediction)
-            plt.axis('off')
-            plt.show()
+            self.image_on_d = self.d.create_image(100, 100, anchor="nw", image=img)
             self.first_prediction = False
+            self.root.mainloop()
         else:
-            plotting.set_data(prediction)
-            plt.draw()
+            self.d.itemconfig(self.image_on_d, image=img)
+            self.root.mainloop()
 
     def reset(self, event):
         self.old_x, self.old_y = None, None
@@ -139,4 +115,26 @@ class Paint(object):
 
 
 if __name__ == '__main__':
-    Paint()
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--model_name', type=str, default='fruits7')
+    args = parser.parse_args()
+    model_name = 'models/'+args.model_name
+    config = tf.ConfigProto()
+    config.gpu_options.allow_growth = True  # dynamically grow the memory used on the GPU
+    config.log_device_placement = True  # to log device placement (on which device the operation ran)
+    sess = tf.Session(config=config)
+    set_session(sess)
+    # loading the model:
+    with open(model_name+'.json', 'r') as file:
+        loaded_json = file.read()
+    # clean json:
+    loaded_json = loaded_json.replace(", \"interpolation\": \"nearest\"", '')
+    loaded_json = loaded_json.replace(", \"output_padding\": null", '')
+    loaded_json = loaded_json.replace(", \"ragged\": false", '')
+    loaded_json = loaded_json.replace(", \"kernel_initializer\": {\"class_name\": \"GlorotUniform\", \"config\": " + \
+                                      "{\"seed\": null, \"dtype\": \"float32\"}}", '')
+    loaded_model = model_from_json(loaded_json)
+    loaded_model.load_weights(model_name+'.h5')
+    loaded_model.compile(loss='mse', optimizer='adam')
+    test = loaded_model.predict(np.random.rand(200 * 200).reshape(1, 200, 200, 1))
+    Paint(loaded_model)
